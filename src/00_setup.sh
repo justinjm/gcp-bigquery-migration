9f42448625c135529c9f63cb842dfb13660924f7
Justin Marciszewski <justinjm@google.com>
2023-02-16 17:06:05 -0500

    update to dataset with 201 rows
    loan_201

--- src/00_setup.sh
+++ src/00_setup.sh
@@ -25,7 +25,7 @@ gcloud projects add-iam-policy-binding ${PROJECT_ID} --member serviceAccount:$SV
 
 ## copy data into GCS bucket ------------------------------------------------------------------
 curl https://raw.githubusercontent.com/justinjm/gcp-bigquery-migration/main/data/loans.sql | gsutil cp - gs://${BUCKET}/loans.sql
-curl https://raw.githubusercontent.com/justinjm/gcp-bigquery-migration/main/data/loan_200k.csv | gsutil cp - gs://${BUCKET}/loan_200k.csv
+# curl https://raw.githubusercontent.com/justinjm/gcp-bigquery-migration/main/data/loan_200k.csv | gsutil cp - gs://${BUCKET}/loan_200k.csv
 curl https://raw.githubusercontent.com/justinjm/gcp-bigquery-migration/main/data/loan_201.csv | gsutil cp - gs://${BUCKET}/loan_201.csv
 
 
