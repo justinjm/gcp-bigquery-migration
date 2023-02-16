6e88191d9e072513eecbf7df77fafbcf26ca0229
Justin Marciszewski <justinjm@google.com>
2023-02-16 13:02:03 -0500

    first test

--- src/04_install_dvt.sh
+++ src/04_install_dvt.sh
@@ -25,7 +25,9 @@ sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
 # optional: for bcp and sqlcmd
 sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+deactivate 
 source ~/.bashrc
+source env/bin/activate
 # optional: for unixODBC development headers
 sudo apt-get install -y unixodbc-dev
 # optional: kerberos library for debian-slim distributions
