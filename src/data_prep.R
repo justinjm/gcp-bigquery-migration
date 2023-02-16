db53fd957cc4ab1c1f44baa1468131b363a7e90d
Justin Marciszewski <justinjm@google.com>
2023-02-16 15:29:16 -0500

    creating smaller test dataset for quick testing
    * file in repo
    * load to GCS (manually)
    * create table in BQ from GCS (UI)

--- src/data_prep.R
+++ src/data_prep.R
@@ -2,13 +2,21 @@ library(here)
 
 infile <- here("data", "loan_200k.csv")
 outfile <- here("data", "loan_200.csv")
+outfile_201 <- here("data", "loan_201.csv")
 
 data_raw <- read.csv(file = infile)
+names(data_raw)
 
-data_out <- data[,!names(data_raw) %in% c("default")]
-
+data_out <- data_raw[, !names(data_raw) %in% c("default")]
 names(data_out)
 
 write.csv(data_out,
           file = outfile,
+          row.names = FALSE)
+
+data_out_201 <- data_out[1:200,]
+nrow(data_out_201)
+
+write.csv(data_out_201,
+          file = outfile_201,
           row.names = FALSE)
